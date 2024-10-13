6b4edfa92267c51510871e433ec966f3e88f10df
Dinko Azema <rocket4ce@gmail.com>
2024-10-12 21:15:05 -0300

    add: generate phx.gen.auth

--- lib/chat_morse_japanese_kana_web/router.ex
+++ lib/chat_morse_japanese_kana_web/router.ex
@@ -1,6 +1,8 @@
 defmodule ChatMorseJapaneseKanaWeb.Router do
   use ChatMorseJapaneseKanaWeb, :router
 
+  import ChatMorseJapaneseKanaWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule ChatMorseJapaneseKanaWeb.Router do
     plug :put_root_layout, html: {ChatMorseJapaneseKanaWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
@@ -39,6 +42,44 @@ defmodule ChatMorseJapaneseKanaWeb.Router do
 
       live_dashboard "/dashboard", metrics: ChatMorseJapaneseKanaWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", ChatMorseJapaneseKanaWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{ChatMorseJapaneseKanaWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", ChatMorseJapaneseKanaWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{ChatMorseJapaneseKanaWeb.UserAuth, :ensure_authenticated}] do
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", ChatMorseJapaneseKanaWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{ChatMorseJapaneseKanaWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
     end
   end
 end
